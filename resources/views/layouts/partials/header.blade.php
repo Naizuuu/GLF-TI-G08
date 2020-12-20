172a58fe725e380bd4619702c2a9345782a2722b
Sebastián <sebazmo@gmail.com>
2020-12-20 04:19:07 -0300

    Cambios menores.

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -4,7 +4,8 @@
         <h3 class="masthead-brand">Empresa D</h3>
         <nav class="nav nav-masthead justify-content-center">
             <a class="nav-link {{request()->routeIs('index') ? 'active' : ''}}" href="{{route('index')}}">Inicio</a>
-            <a class="nav-link disabled {{request()->routeIs('hoja_de_ruta') ? 'active' : ''}}" href="{{route('hoja_de_ruta')}}">Hoja de Ruta</a> 
+            <a class="nav-link disabled {{request()->routeIs('hoja_de_ruta') ? 'active' : ''}}" href="{{route('hoja_de_ruta')}}">Hoja de Ruta</a>
+            <a class="nav-link disabled {{request()->routeIs('hoja_de_ruta_proceso') ? 'active' : ''}}" href="{{route('hoja_de_ruta_proceso')}}">Proceso</a> 
         </nav>
     </div>
 </header>
